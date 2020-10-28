d6f29d8de7b06c6c0fba67d58595046b3582361d
ZakirBangash <engrzakirbangash@gmail.com>
2020-10-28 16:10:35 +0500

    Routing
    Will continue from routing inshallah

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
 import Header from './Header';
+import Chats from './Chats'
+import {
+  Routes,
+  Route
+} from 'react-router-dom';
 
 function App() {
   return (
     <div className="app">
-     <Header />
+     
+     <Routes>
+          <Route path="/" element={<Header  />} /> 
+          <Route path="/chats" element={<Header backButton="/" />} /> 
+
+       
+      </Routes>
     </div>
   );
 }
---
+++ src/Chats.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Chats = () => {
+    console.log("Good im render here");
+    return (
+        <div className="chats">
+            <h1>I am from Chat sections</h1>
+        </div>
+    )
+}
+
+export default Chats
